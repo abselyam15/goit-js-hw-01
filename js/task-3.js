b5add0a99257f62e26dca4fc17771dbf63e01633
abselyam15 <99846458+abselyam15@users.noreply.github.com>
2024-01-31 20:17:50 +0100

    done

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+function getElementWidth(content, padding, border) {
+    const firstNum = Number.parseFloat(content)
+    const secondNum = Number.parseFloat(padding)
+    const thirdNum = Number.parseFloat(border)
+    // console.log(firstNum, secondNum, thirdNum)
+    const sum = firstNum + secondNum * 2 + thirdNum * 2
+    console.log(sum)
+}
+
+getElementWidth("50px", "8px", "4px") // 74
+getElementWidth("60px", "12px", "8.5px"); // 101
+getElementWidth("200px", "0px", "0px"); // 200
