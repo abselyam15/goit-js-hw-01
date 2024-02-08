78721d4d4c984f2e4115fbef442b1ba12a2f64ff
abselyam15 <99846458+abselyam15@users.noreply.github.com>
2024-02-08 21:22:14 +0100

    Fix pathes to scripts

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,11 @@
 function getElementWidth(content, padding, border) {
-    const firstNum = Number.parseFloat(content)
-    const secondNum = Number.parseFloat(padding)
-    const thirdNum = Number.parseFloat(border)
-    // console.log(firstNum, secondNum, thirdNum)
-    const sum = firstNum + secondNum * 2 + thirdNum * 2
-    console.log(sum)
+	const firstNum = Number.parseFloat(content)
+	const secondNum = Number.parseFloat(padding)
+	const thirdNum = Number.parseFloat(border)
+	const sum = firstNum + secondNum * 2 + thirdNum * 2
+	console.log(`[TASK 3]: ${sum}`)
 }
 
-getElementWidth("50px", "8px", "4px") // 74
-getElementWidth("60px", "12px", "8.5px"); // 101
-getElementWidth("200px", "0px", "0px"); // 200
+getElementWidth('50px', '8px', '4px') // 74
+getElementWidth('60px', '12px', '8.5px') // 101
+getElementWidth('200px', '0px', '0px') // 200
